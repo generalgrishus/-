57ab26756a2eb9cc7c305c30b6c1768064bf4f5f
Sofia <sofia.sofia>
2023-05-04 15:28:41 +0500

    Добавила модель Place в приложении places

--- Backend/places/models.py
+++ Backend/places/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Place(models.Model):
+    lon = models.DecimalField("longitude", max_digits=9, decimal_places=6)
+    lat = models.DecimalField("latitude", max_digits=9, decimal_places=6)
+    description = models.TextField(blank=True, null=True)
+    #image =
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_visited = models.BooleanField(default=False)

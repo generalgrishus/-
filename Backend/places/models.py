32ce5a05827970bc4ca0d16ba9ca7395cf3e6449
Sofia <sofia.sofia>
2023-05-04 16:36:09 +0500

    Изменила поле id на uuid в Place

--- Backend/places/models.py
+++ Backend/places/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
 class Place(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True) 
     lon = models.DecimalField("longitude", max_digits=9, decimal_places=6)
     lat = models.DecimalField("latitude", max_digits=9, decimal_places=6)
     description = models.TextField(blank=True, null=True)

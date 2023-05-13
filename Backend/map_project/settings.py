84c7c9d13e8b2958ccc2e8f645ebf388ce79cf90
Sofia <sofia.sofia>
2023-05-13 15:11:13 +0500

    Добавила users в INSTALLED_APPS

--- Backend/map_project/settings.py
+++ Backend/map_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'places.apps.PlacesConfig',
     'django.contrib.admin',
     'django.contrib.auth',

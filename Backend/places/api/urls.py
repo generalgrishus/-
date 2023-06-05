ad8a2cccba72b8f0553a495b5e3b56a06526f744
Sofia <sofia.sofia>
2023-06-05 17:16:02 +0500

    Добавила get метод в api метод выводит все объекты карты, принадлежащие пользователю

--- Backend/places/api/urls.py
+++ Backend/places/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add/', views.PlaceAPIView.as_view(), name="add_place"),
+    path('add/', views.PlaceListView.as_view(), name="add_place"),
+    path('get/', views.PlaceListView.as_view(), name="get_places")
 ]
--- Backend/places/api/views.py
+++ Backend/places/api/views.py
@@ -4,10 +4,15 @@ from .serializers import PlaceSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-class PlaceAPIView(APIView):
+class PlaceListView(APIView):
     def post(self, request, format=None):
         serializer = PlaceSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request, format=None):
+        places = Place.objects.filter(creator=request.user.id)
+        serializer = PlaceSerializer(places, many=True)
+        return Response(serializer.data)

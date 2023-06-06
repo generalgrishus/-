afcc7eff63c9dd289f4a67ef86f57617e1a1d997
Sofia <sofia.sofia>
2023-06-06 22:16:35 +0500

    Изменения в поведении view set Нельзя менять координаты объекта после добавления Нельзя добавлять объект от имени не авторизованного в текущий момент пользователя

--- Backend/places/api/views.py
+++ Backend/places/api/views.py
@@ -1,15 +1,23 @@
-from places.models import Place
-from .serializers import PlaceSerializer
+from .serializers import PlaceSerializer, PlaceWithoutLonLatSerializer
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class PlaceViewSet(viewsets.ModelViewSet):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     serializer_class = PlaceSerializer
     lookup_field = 'pk'
     lookup_value_regex = '[0-9a-f]{32}'
     
     def get_queryset(self):
         return self.request.user.place_set.all()
+
+    def get_serializer_class(self):
+        serializer_class = self.serializer_class
+
+        if self.request.method == 'PUT':
+            serializer_class = PlaceWithoutLonLatSerializer
+        return serializer_class

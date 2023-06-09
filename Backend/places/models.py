8d70e0142ef3cf7b73904313acefffc1701680d6
Sofia <sofia.sofia>
2023-06-09 12:03:56 +0500

    Поменяла местами lat и lon в Place модели

--- Backend/places/models.py
+++ Backend/places/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class Place(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=200, blank=True)
-    lon = models.DecimalField("longitude", max_digits=9, decimal_places=6)
     lat = models.DecimalField("latitude", max_digits=9, decimal_places=6)
+    lon = models.DecimalField("longitude", max_digits=9, decimal_places=6)
     description = models.TextField(blank=True)
     #image =
     creator = models.ForeignKey(User, on_delete=models.CASCADE)

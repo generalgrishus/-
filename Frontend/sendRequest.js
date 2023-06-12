e888eef6f319557521232578d42b906076a8e11e
generalgrishus <131965815+generalgrishus@users.noreply.github.com>
2023-06-12 14:06:32 +0300

    Изменения сайта
    Добавлена регистрация, авторизация, добавлены анимации, новые стили для кнопок

--- Frontend/sendRequest.js
+++
@@ -1,9 +0,0 @@
-import React, {useState, useEffect} from "react";
-import axios, {isCancel, AxiosError} from 'axios';
-
-const Reg = () => {
-
-  useEffect(() => {
-    console.log("useEffect");
-  })
-}

1bff333f4c83ebdc239023d85df59dcb30bb9500
generalgrishus <131965815+generalgrishus@users.noreply.github.com>
2023-06-16 20:10:20 +0300

    Добавлены страницы достопримечательностей, карта разбита по слоям для фильтров, добавлено специальное окно для пользовательского места

--- indexMap.js
+++ indexMap.js
@@ -1,7 +1,7 @@
 async function getGETJsonAsync() {
   const response = await getGETResponseAsync()
   if (response.status != 200)
-    window.location.href = "authorizationPage.html"
+    window.location.href = "registerPage.html"
   const jsonData = await response.json()
   return jsonData
 }
@@ -56,9 +56,10 @@ async function setPersonalMarkers(map, customIcon) {
   for (let i = 0; i < jsonData.length; i++) {
     let obj = jsonData[i];
     const coordinates = [obj.lat, obj.lon]
-    const marker = L.marker(coordinates, { icon: customIcon }).addTo(layer2);
+    const marker = L.marker(coordinates, { icon: customIcon }).addTo(layer7);
     marker._leaflet_id = obj.id;
-    marker.bindPopup(createPlaceForm(marker, obj.name, obj.description, obj.is_visited, isPersonalMarker = true));
+    marker.bindPopup(createPlaceForm(marker, obj.name, obj.description, obj.is_visited, isPersonalMarker = true), {className: 'user-popup'});
+
   }
 }
 
@@ -70,7 +71,7 @@ async function sendPersonalMarkerInfo(marker, data) {
     if (response.status == 201) {
       marker._leaflet_id = json.id;
       map.closePopup();
-      marker.setIcon(customIcon1);
+      marker.setIcon(userIcon);
     }
   }
   else {
@@ -100,7 +101,7 @@ function createPlaceForm(marker, name, description, isVisited = false, isPersona
     .prop("checked", isVisited);
 
   $(form).attr("class", "placeForm")
-    .append("<h1>Название: </h1>")
+    .append("<h1>Название:</h1>")
     .append(NAME)
     .append("<br>")
     .append("<h1>Описание: </h1>")
@@ -150,14 +151,45 @@ var popup = L.popup({
   autoClose: true,
   className: "custom-popup"
 })
+var userPopup = L.popup({
+  closeButton: true,
+  autoClose: true,
+  className: "user-popup"
+})
 var customIcon1 = L.icon({
-  iconUrl: 'img/icon1.png',
-  iconSize: [37, 51],
+  iconUrl: 'img/pinkIcon.png',
+  iconSize: [25, 39],
   popupAnchor: [-340, 450]
 });
+var customIcon2 = L.icon({
+  iconUrl: 'img/blueIcon.png',
+  iconSize: [25, 39],
+  popupAnchor: [-340, 450]
+});
+var customIcon3 = L.icon({
+  iconUrl: 'img/orangeIcon.png',
+  iconSize: [25, 39],
+  popupAnchor: [-340, 450]
+});
+var customIcon4 = L.icon({
+  iconUrl: 'img/greenIcon.png',
+  iconSize: [25, 39],
+  popupAnchor: [-340, 450]
+});
+var customIcon5 = L.icon({
+  iconUrl: 'img/yellowIcon.png',
+  iconSize: [25, 39],
+  popupAnchor: [-340, 450]
+});
+var customIcon6 = L.icon({
+  iconUrl: 'img/purpleIcon.png',
+  iconSize: [25, 39],
+  popupAnchor: [-340, 450]
+});
+
 var userIcon = L.icon({
   iconUrl: 'img/userIcon.png',
-  iconSize: [37, 51],
+  iconSize: [25, 39],
   popupAnchor: [-340, 390]
 });
 
@@ -171,37 +203,51 @@ L.tileLayer('https://api.mapbox.com/styles/v1/generalgrishus/clhmfa98c01qx01p6ci
 }).addTo(map);
 map.zoomControl.setPosition('bottomleft');
 var layer1 = L.geoJSON(null, { layerName: 'layer1' }).addTo(map);
-var touristPoint = L.marker([56.83845, 60.60332], { icon: customIcon1 }).addTo(layer1);
-touristPoint.bindPopup('Мост "Плотинка"');
-var layer2 = L.geoJSON(null, { layerName: 'layer2' }).addTo(map);
-(async() => {await setPersonalMarkers(map, customIcon1)})();
-var marker = L.marker([56.84387, 60.65367], { icon: customIcon1 }).addTo(layer2);
-marker.bindPopup(popup);
-marker = L.marker([56.83242, 60.60744], { icon: customIcon1 }).addTo(layer2);
-marker.bindPopup('Памятник "Клавиатуре"');
-marker = L.marker([56.85878, 60.59911], { icon: customIcon1 }).addTo(layer2);
-marker.bindPopup(
-
-)
-marker = L.marker([56.82878, 60.59911], { icon: customIcon1 }).addTo(layer2);
-marker.bindPopup('Торговый центр "Гринвич"');
-marker = L.marker([56.84638, 60.67985], { icon: customIcon1 }).addTo(layer2);
-marker.bindPopup('Шарташские каменные палатки');
-// marker = L.marker([56.83242, 60.60744], {icon : customIcon1 }).addTo(map);
-// marker.bindPopup('Памятник "Клавиатуре"');
-// marker = L.marker([56.83242, 60.60744], {icon : customIcon1 }).addTo(map);
-// marker.bindPopup('Памятник "Клавиатуре"');
-// marker = L.marker([56.83242, 60.60744], {icon : customIcon1 }).addTo(map);
-// marker.bindPopup('Памятник "Клавиатуре"');
-marker = L.marker([56.8146, 60.6432], { icon: customIcon1 }).addTo(layer2);
-marker.bindPopup("Парк имени Маяковского");
-touristPoint = L.marker([56.83598, 60.61456], { icon: customIcon1 }, {
+var interestingPlace = L.marker([56.83845, 60.60332], { icon: customIcon1 }).addTo(layer1);
+interestingPlace.bindPopup('Мост "Плотинка"',  {className: 'custom-popup'});
+marker = L.marker([56.8146, 60.6432], { icon: customIcon1 }).addTo(layer1);
+marker.bindPopup('Парк Маяковского', {className: 'custom-popup'});
+interestingPlace = L.marker([56.83598, 60.61456], { icon: customIcon1 }, {
   title: "Бизнес-центр Высоцкий",
-}).addTo(layer2);
-touristPoint.bindPopup(
-  '<div class = "sightWindow"><img class = "vysotskyWindow" src="img/высоцкий.jpg">' +
-  "<div class = 'sightName'><h2>Смотровая площадка бизнес-центра Высоцкого</h2></div>" +
-  "<p></p>" +
-  "<i><b><a href='VysotskyPage.html'></a></b></i>" +
-  "</div>");
+}).addTo(layer1);
+interestingPlace.bindPopup(
+  '<div class = "sightWindow"><img class = "vysotskyWindow" src="img/смотрПлощ.jpg">' +
+  '<div><a class = "sightLink" href="VysotskyPage.html"><i><h2 class = "sightName">Смотровая площадка бизнес центра "Высоцкий"</h2></i></a></div><br>' +
+  "<div><span class = 'sightInf'><p>Открытая смотровая площадка расположена в Бизнес Центре «Высоцкий» на 52 этаже — на высоте 186 метров. Отсюда взору открывается поистине фантастический вид: панорама Екатеринбурга простирается вдаль на 25 километров.</p></span></div>" +
+  "</div>", {className: 'custom-popup'});
+var layer2 = L.geoJSON(null, { layerName: 'layer2' }).addTo(map);
+var marker = L.marker([56.84387, 60.65367], { icon: customIcon2 }).addTo(layer2);
+marker.bindPopup(popup, {className: 'custom-popup'});
+var layer3 = L.geoJSON(null, { layerName: 'layer3' }).addTo(map);
+marker = L.marker([56.83242, 60.60744], { icon: customIcon3 }).addTo(layer3);
+marker.bindPopup('Памятник "Клавиатуре"', {className: 'custom-popup'});
+var layer4 = L.geoJSON(null, { layerName: 'layer4' }).addTo(map);
+var park = L.marker([56.82910,60.60362], { icon: customIcon4 }).addTo(layer4);
+park.bindPopup(
+  '<div class = "sightWindow"><img class = "popupWindow" src="img/дендроПарк.jpg">' +
+'<div><a class = "sightLink" href="dendroPark.html"><i><h2 class = "sightName1">Дендропарк</h2></i></a></div><br>' +
+"<div><span class = 'sightInf'><p>Екатеринбургский дендропарк включает в себя два парка в Екатеринбурге, один из которых находится на пересечении улиц Мира и Первомайской в Кировском районе (Дендрологический парк), второй — на пересечении улиц 8 Марта и Куйбышева в Ленинском районе (Дендропарк).</p></span></div>" +
+"</div>", {className: 'custom-popup'})
+park = L.marker([56.84638, 60.67985], { icon: customIcon4 }).addTo(layer4);
+park.bindPopup(
+  '<div class = "sightWindow"><img class = "popupWindow" src="img/shartash.jpg" width = "400px" height = "250px">' +
+'<div><a class = "nameSight" href="shartash.html"><i><h2 class = "sightName">Каменные палатки</h2></i></a></div><br>' +
+"<div><span class = 'sightInf'><p>Шарташские каменные палатки — уникальный памятник природы, что в 2 км от самого центра Екатеринбурга. За забавным именем скрываются огромные скалистые образования, возникшие много лет назад. Название происходит от редких матрацевидных форм скал, которую они приобрели благодаря выветриванию гранитных пород. Шарташскими же они называются из-за близости к озеру Шарташ, расположенному всего в километре. </p></span></div>" +
+"</div>", {className: 'custom-popup'})
+var layer5 = L.geoJSON(null, { layerName: 'layer5' }).addTo(map);
+marker = L.marker([56.82878, 60.59911], { icon: customIcon5 }).addTo(layer5);
+marker.bindPopup('Торговый центр "Гринвич"', {className: 'custom-popup'});
+var layer6 = L.geoJSON(null, { layerName: 'layer6' }).addTo(map);
+marker = L.marker([56.84638, 60.99985], { icon: customIcon6 }).addTo(layer6);
+marker.bindPopup('Шарташские каменные палатки', {className: 'custom-popup'});
+var layer7 = L.geoJSON(null, { layerName: 'layer7' }).addTo(map);
+(async() => {await setPersonalMarkers(map, userIcon)})();
+
+// marker = L.marker([56.83242, 60.60744], {icon : customIcon1 }).addTo(map);
+// marker.bindPopup('Памятник "Клавиатуре"');
+// marker = L.marker([56.83242, 60.60744], {icon : customIcon1 }).addTo(map);
+// marker.bindPopup('Памятник "Клавиатуре"');
+// marker = L.marker([56.83242, 60.60744], {icon : customIcon1 }).addTo(map);
+// marker.bindPopup('Памятник "Клавиатуре"');
+
 
